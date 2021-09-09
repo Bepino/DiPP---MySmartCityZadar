db54a4810c3593fb9f48ccaf70f5f1009a76ad4c
Bepino <yak789456.89@gmail.com>
2021-09-09 17:18:14 +0200

    addon

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import './Profile.css'
 
 class Profile extends React.Component {
     render(){ return(
         <div id="base">
             <div id="banner"></div>
-            <div id="userinfo"></div>
+            <div id="userinfo">
+                <div id="picture-name">
+                    <img src="" alt="Logo.png" />
+                    <p id="name">Marin Cilic</p>
+                    <p id="tag"></p>
+                </div>
+                <div id="social"></div>
+                <div id="contact-info"> </div>
+            </div>
             <div id="portfolio"></div>
             <div id="projects"></div>
-            Profile page
+
         </div>
     )};
 }
