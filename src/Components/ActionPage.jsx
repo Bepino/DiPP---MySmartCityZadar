dd94dfff5ed9cd31a3427d9d7384ed3337976b86
Bepino <yak789456.89@gmail.com>
2021-09-09 19:10:17 +0200

    pnis

--- src/Components/ActionPage.jsx
+++ src/Components/ActionPage.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 class ActionPage extends React.Component {
-    render(){ return(
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event){
+        var text = 'pemis \n';
+            var pemisCounter = 0;
+            for(pemisCounter; pemisCounter<1000;pemisCounter++){
+                text += 'pemis \n';
+            }
+            ReactDOM.render(<div>{text}</div>, document.getElementById('root'))
+    }
+
+    render(){ 
+        
+        return(
         <div id="main">
-            Hi there buddy ol' pal
+            <button onClick={this.handleClick()}></button>
         </div>
     )};
 }
