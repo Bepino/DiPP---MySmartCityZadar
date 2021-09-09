8644701750cd1ba9acac8c561be7fd02c4310c10
Bepino <yak789456.89@gmail.com>
2021-09-09 16:08:37 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ActionPage from './Components/ActionPage'
+import Profile from './Components/Profile'
+
 function App() {
 
   return (
     <Router>
       <Route exact path="/ActionPage" component={ActionPage}/>
+      <Route exact path="/Profile" component={Profile}/>
     </Router>
   );
 }
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class Profile extends React.Component {
+    render(){ return(
+        <div id="base">
+            <div id="banner"></div>
+            <div id="userinfo"></div>
+            <div id="portfolio"></div>
+            <div id="projects"></div>
+            Profile page
+        </div>
+    )};
+}
+export default Profile;
